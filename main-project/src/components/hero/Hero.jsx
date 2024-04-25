c3c342b63ec49a7658030bfa6797909f45a7fa67
tanvirtbf <tanver018765@gmail.com>
2024-04-25 10:08:55 +0600

    start next section

--- main-project/src/components/hero/Hero.jsx
+++ main-project/src/components/hero/Hero.jsx
@@ -1,16 +1,24 @@
-import styles from './Hero.module.css'
 import { FaLongArrowAltRight } from "react-icons/fa";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <div className={`${styles.hero} container`}>
       <div className={styles.heroText}>
         <h1>We Ensure better education for a better world</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus nam alias, dicta at iusto libero obcaecati suscipit vero excepturi ea quia voluptate dolorem quod itaque magnam? Harum possimus voluptatibus repudiandae qui pariatur distinctio. Libero, perspiciatis? Nihil quas excepturi fugit laudantium dolores aliquid </p>
-        <button className="btn">Explore More <FaLongArrowAltRight /></button>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Necessitatibus nam alias, dicta at iusto libero obcaecati suscipit
+          vero excepturi ea quia voluptate dolorem quod itaque magnam? Harum
+          possimus voluptatibus repudiandae qui pariatur distinctio. Libero,
+          perspiciatis? Nihil quas excepturi fugit laudantium dolores aliquid{" "}
+        </p>
+        <button className="btn">
+          Explore More <FaLongArrowAltRight />
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
