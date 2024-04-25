e07b5004e1036af05a02893240328a45572c5717
tanvirtbf <tanver018765@gmail.com>
2024-04-25 17:10:01 +0600

    complete navbar

--- main-project/src/components/navbar/Navbar.jsx
+++ main-project/src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import styles from './Navbar.module.css'
 import Logo from '../../assets/unnamed.jpg'
+import { useEffect, useState } from 'react'
 
 const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false)
+
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+    })
+  },[])
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky ? styles.darkNav : '' }`}>
       <img src={Logo} alt="" className={styles.logo} />
       <ul>
         <li>Home</li>
