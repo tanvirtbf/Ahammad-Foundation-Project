504dcf0f1e547c1b089238fbd404df247cb77ee3
tanvirtbf <tanver018765@gmail.com>
2024-04-27 11:19:04 +0600

    complete contact us basic html code

--- main-project/src/components/contact/Contact.jsx
+++ main-project/src/components/contact/Contact.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from './Contact.module.css'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className={styles.contact}>
+      <div className={styles.contactCol}>
+        <h3>Send us a Message</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nostrum quia est dolores veniam consequuntur voluptatum neque, fugiat optio iure officia incidunt nesciunt, pariatur et debitis cumque quidem velit magni eaque vel delectus aliquam maiores quibusdam! Natus repellat iusto aperiam!</p>
+        <ul>
+          <li>Contact@ahammad2022foundation@gmail.com</li>
+          <li>01317277858</li>
+          <li>Sonir Akhra, Zia-sharani road, dhaka 1236</li>
+        </ul>
+      </div>
     </div>
   )
 }
