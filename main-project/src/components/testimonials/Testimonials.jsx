bafcc79d9464b3f763b7ea9dbae4c0a21cf1d6ae
tanvirtbf <tanver018765@gmail.com>
2024-04-27 10:07:55 +0600

    import all picture for testimonial section and placed it

--- main-project/src/components/testimonials/Testimonials.jsx
+++ main-project/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,16 @@
 import styles from './Testimonials.module.css'
+import nextIcon from '../../assets/next-icon.png'
+import backIcon from '../../assets/back-icon.png'
+import user1 from '../../assets/user-1.png'
+import user2 from '../../assets/user-2.png'
+import user3 from '../../assets/user-3.png'
+import user4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
   return (
-    <div>
-      
+    <div className={styles.testimonials}>
+      <img src={nextIcon} alt="" className={styles.nextBtn}/>
+      <img src={backIcon} alt="" className={styles.backBtn}/>
     </div>
   )
 }
