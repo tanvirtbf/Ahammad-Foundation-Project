2cc069f2915ba40083bb596b8e183bd6fac25d3d
tanvirtbf <tanver018765@gmail.com>
2024-04-29 14:29:45 +0600

    complete navbar responsive

--- main-project/src/components/navbar/Navbar.jsx
+++ main-project/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import Logo from "../../assets/unnamed.jpg";
 import styles from "./Navbar.module.css";
+import { RiMenu3Fill } from "react-icons/ri";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -12,43 +13,54 @@ const Navbar = () => {
     });
   }, []);
 
+  const [mobileMenu,setMobileMenu] = useState(false)
+
+  const handleNavbar = () =>{
+    mobileMenu? setMobileMenu(false): setMobileMenu(true);
+  }
+
+  const toggleMenu = () =>{
+    mobileMenu? setMobileMenu(false): setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? styles.darkNav : ""}`}>
       <img src={Logo} alt="" className={styles.logo} />
-      <ul>
+      <ul className={`${mobileMenu? styles.hideMobileMenuSmooth : styles.hideMobileMenu} `}>
         <li>
-          <Link to="hero" smooth={true} offset={0} duration={500}>
+          <Link to="hero" smooth={true} offset={0} duration={500} onClick={handleNavbar}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="program" smooth={true} offset={-260} duration={500}>
+          <Link to="program" smooth={true} offset={-260} duration={500} onClick={handleNavbar}>
             Program
           </Link>
         </li>
         <li>
-          <Link to="about" smooth={true} offset={-150} duration={500}>
+          <Link to="about" smooth={true} offset={-150} duration={500} onClick={handleNavbar}>
             About Us
           </Link>
         </li>
         <li>
-          <Link to="campus" smooth={true} offset={-260} duration={500}>
+          <Link to="campus" smooth={true} offset={-260} duration={500} onClick={handleNavbar}>
             Campus
           </Link>
         </li>
         <li>
-          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+          <Link to="testimonials" smooth={true} offset={-260} duration={500} onClick={handleNavbar}>
             Testimonial
           </Link>
         </li>
         <li>
           <button className="btn">
-            <Link to="contact" smooth={true} offset={-260} duration={500}>
+            <Link to="contact" smooth={true} offset={-260} duration={500} onClick={handleNavbar}>
               Contact Us
             </Link>
           </button>
         </li>
       </ul>
+      <RiMenu3Fill className={styles.menuIcon} onClick={toggleMenu} />
     </nav>
   );
 };
